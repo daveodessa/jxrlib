40a2a7560008303b3410e54ad2289f3ea9202a73
daveodessa <47123626+daveodessa@users.noreply.github.com>
2020-10-14 10:57:07 +0300

    Update JXRGlueJxr.c

--- jxrgluelib/JXRGlueJxr.c
+++ jxrgluelib/JXRGlueJxr.c
@@ -1,15 +1,15 @@
 
 //*@@@+++@@@@******************************************************************
 //
-// Copyright © Microsoft Corp.
+// Copyright ï¿½ Microsoft Corp.
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 // 
-// • Redistributions of source code must retain the above copyright notice,
+// ï¿½ Redistributions of source code must retain the above copyright notice,
 //   this list of conditions and the following disclaimer.
-// • Redistributions in binary form must reproduce the above copyright notice,
+// ï¿½ Redistributions in binary form must reproduce the above copyright notice,
 //   this list of conditions and the following disclaimer in the documentation
 //   and/or other materials provided with the distribution.
 // 
@@ -724,7 +724,7 @@ ERR WriteContainerPost(
         offPos = pDEMisc->uOffAlphaOffset;
         Call(WriteWmpDE(pWS, &offPos, &deAlphaOffset, NULL, NULL));
 
-        deAlphaByteCount.uValueOrOffset = pIE->WMP.nCbAlpha + pIE->WMP.nOffAlpha;
+        deAlphaByteCount.uValueOrOffset = pIE->WMP.nCbAlpha;
         offPos = pDEMisc->uOffAlphaByteCount;
         Call(WriteWmpDE(pWS, &offPos, &deAlphaByteCount, NULL, NULL));
     }
